7e7e7e76ce6688c964768da3d74b106f2ba118eb
Oscar Elmqvist Sandvik <oscar.elmqvistsandvik@gmail.com>
2015-06-29 16:38:38 +0200

    Now the equations for the quadrupoles are correct

--- lie.py
+++ lie.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.misc import *
 from sympy.parsing.sympy_parser import parse_expr
 from sympy import *
+#printing.init_printing(use_latex='mathjax') # latex output in ipython notebook
 #from sympy import Function, Symbol, symbols, summation
 #from sympy.mpmath import *
 
@@ -33,6 +34,7 @@ m = Symbol('m') # arbitrary mass
 q = Symbol('q')
 g = Symbol('g')
 l = Symbol('l') # arbitrary length
+k = Symbol('k')
 
 print('- Lie calculations...\n')
 # variable definitions
@@ -82,17 +84,23 @@ def lieop(f,g):
 
 
 ## Lie transformation
+# old version
 def lietransform(ham, vof0, t, order):
     voft = vof0
     for i in range(1,order+1):
-        lieterm = lieop(ham,vof0)
+        lieterm = simplify(lieop(ham,vof0))
         for j in range(0,i-1):
-            lieterm = lieop(ham,lieterm)
+            lieterm = simplify(lieop(ham,lieterm))
 
         #print "lieterm:",lieterm
-        voft = voft - t**i / float(factorial(i)) * lieterm
+        #voft = voft + t**i / factorial(i) * lieterm # for my formalism
+        voft = voft + lieterm / factorial(i) # for Ems formalism
 
     return voft
+
+# new version
+#def lietransform(ham, vof0, t, order):
+#    return
 
 # x = mm, v = mum
 #ham = p**2 / (2*m)
@@ -106,28 +114,31 @@ def lietransform(ham, vof0, t, order):
 ## elementary elements
 # drift
 print "Drift..."
-driftham = px**2 / (2*m) + py**2 / (2*m) + pz**2 / (2*m)
+#driftham = px**2 / (2*m) + py**2 / (2*m) + pz**2 / (2*m) # for my formalism
+driftham = -l/2*(px**2 + py**2 + pz**2)
 
 #t = 10
 t = Symbol('t')
-order = 3
+order = 5
 
 transresultqx = lietransform(driftham, v[0], t, order)
-print "transresultqx:", transresultqx
+#print "transresultqx:", transresultqx
 transresultpx = lietransform(driftham, v[3], t, order)
-print "transresultpx:", transresultpx
+#print "transresultpx:", transresultpx
 
 # substitution
 def substitution(expr):
     expr = expr.subs(t, l*m/pz)
     expr = expr.subs(qx, x0)
     expr = expr.subs(px/pz, xprime)
+    expr = expr.subs(q*g/pz,k**2)
     return expr
 
 xofl = substitution(transresultqx)
 print "xofl:", xofl
 
-xprimeofl = substitution(transresultpx/pz)
+#xprimeofl = substitution(transresultpx/pz) # for my formalism
+xprimeofl = substitution(transresultpx) # for Ems formalism
 print "xprimeofl:", xprimeofl
 
 
@@ -165,7 +176,7 @@ def removeprefixones(expr): # still in an early form, BUG: it removes 1.0 if the
     newexpr = parse_expr(strexpr)
     return newexpr
 
-# Matrix construction
+# Matrix construction, only works with my formalism
 print "Matrix construction..."
 m11 = removeprefixones(selectterm(xofl,x0))
 m12 = removeprefixones(selectterm(xofl,xprime))
@@ -180,23 +191,26 @@ print driftmatrix
 
 # quad
 print "Quad..."
-quadham = px**2 / (2*m) + py**2 / (2*m) + pz**2 / (2*m) + q*g*pz*qx**2 / (2*m) - q*g*pz*qy**2 / (2*m) # for some reason the denominator always has to be last in the term
+#quadham = px**2 / (2*m) + py**2 / (2*m) + pz**2 / (2*m) + q*g*pz*qx**2 / (2*m) - q*g*pz*qy**2 / (2*m) # for some reason the denominator always has to be last in the term. for my formalism
+quadham = -l/2*(k**2*(qx**2+qy**2+qz**2)+px**2+py**2+pz**2) # for Ems formalism, replace k with -k for defocus
 
 #t = 10
 t = Symbol('t')
-order = 6
+order = 5
 
 transresultqx = lietransform(quadham, v[0], t, order)
-print "transresultqx:", transresultqx
+#print "transresultqx:", transresultqx
 transresultpx = lietransform(quadham, v[3], t, order)
-print "transresultpx:", transresultpx
+#print "transresultpx:", transresultpx
 
 # substitution
 xofl = substitution(transresultqx)
 #fourier() here to go from taylor to sin & cos ?
 print "xofl:", xofl
 
-xprimeofl = substitution(transresultpx/pz)
+#xprimeofl = simplify(transresultpx/pz) # for my formalism
+xprimeofl = simplify(transresultpx) # for Ems formalism
+xprimeofl = substitution(xprimeofl)
 print "xprimeofl:", xprimeofl
 
 #for the other dimension replace x with y and z, will make functions for this later
