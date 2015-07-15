fc6e43f8fd8acd89603a1e1941def55fbccc5a9b
Oscar Elmqvist Sandvik <oscar.elmqvistsandvik@gmail.com>
2015-07-15 12:35:16 +0200

    Added a short program for comparing results

---
+++ diff.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+##### Diff mellan mina o antons resultat, datan ska va sparad som x column xp column
+antonsdata = raw_input('Enter anton\'s datafile name:')
+if len(antonsdata) < 1 : antonsdata = "out.txt"
+oscarsdata = raw_input('Enter oscar\'s datafile name:')
+if len(oscarsdata) < 1 : oscarsdata = "out.txt"
+
+antonx, antonxp = np.loadtxt(antonsdata,unpack = True)
+oscarx, oscarxp = np.loadtxt(oscarsdata,unpack = True)
+
+diffx = antonx - oscarx
+diffxp = antonxp - oscarxp
+
+print 'diffx:',diffx
+print 'diffxp:',diffxp
+
+stdx = sum(diffx**2) - sum(diffx)**2
+stdxp = sum(diffxp**2) - sum(diffxp)**2
+
+print 'stdx:',stdx
+print 'stdxp:',stdxp
