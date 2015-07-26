79b64e93b67b8b6d35f75ab2b97ac9d00c7b1014
Oscar Elmqvist Sandvik <oscar.elmqvistsandvik@gmail.com>
2015-07-26 15:26:31 +0200

    Cleaned out old and unused code

--- lie.py
+++ lie.py
@@ -33,44 +33,18 @@ yprime = Symbol('yprime')
 z = Symbol('z')
 zprime = Symbol('zprime')
 
-## Needed for my old formalism, zzz
-#x0 = Symbol('x0')
-#xprime0 = Symbol('xprime0')
-#y0 = Symbol('y0')
-#yprime0 = Symbol('yprime0')
-#z0 = Symbol('z0')
-#zprime0 = Symbol('zprime0')
-
-##m = 1.67262178*10**-27 # mass of proton
-#m = Symbol('m') # arbitrary mass
-#
-#q = Symbol('q')
-#p = Symbol('p')
-#g = Symbol('g')
-#t = Symbol('t')
-## end zzz
-
 
 l = Symbol('l') # arbitrary length
 k = Symbol('k')
 
 
 ## Hamiltonians
-#ems way
 driftham = -l/2*(px**2 + py**2 + pz**2)
 quadham = -l/2*(k**2*(qx**2-qy**2)+px**2+py**2+pz**2) # (Ems formalism), replace k with -k for defocus. Without quad term in z dir
 quadhamdefocus = -l/2*(-k**2*(qx**2-qy**2)+px**2+py**2+pz**2) # (Ems formalism), replace k with -k for defocus. Without quad term in z dir
 sextupoleham = -l/2*(2/3*k*(qx**3-3*qx*qy**2)+(px**2+py**2)) # should the ps' perhaps be divided by 2 as in nonlinear2013_3.pdf? That division is assumed to be the l/2 in the beginning, 
 octupoleham = -l/2*(2/4*k*(qx**4-6*qx**2*qy**2+qy**4)+(px**2+py**2)) # same decision as above
 
-#old way
-#driftham = px**2 / (2*m) + py**2 / (2*m) + pz**2 / (2*m) # for my formalism
-#quadham = px**2 / (2*m) + py**2 / (2*m) + pz**2 / (2*m) + q*g*pz*qx**2 / (2*m) - q*g*pz*qy**2 / (2*m) # for some reason the denominator always has to be last in the term. for my formalism
-
-## One can transform arbitrary functions if one would like so
-# arbitrary funs here
-#f = Function('f')(q,p)
-#g = Function('g')(q,p)
 
 ###################################### Operators and transforms here
 def lieop(f,g):
@@ -111,76 +85,6 @@ def lietransform(ham, vof0, order):#,t):, #zzz
     return voft
 
 
-## substitution, only needed for my formalism, zzz
-def substitution(expr):
-
-    ## For my formalism this is need, but for Ems formalism it is not needed
-    ######expr = expr.subs(q*g/pz,k**2) # "Wrong definition" from accelerator recipes
-    #expr = expr.subs(qx, x0)
-    #expr = expr.subs(qy, y0)
-    #expr = expr.subs(px/pz, xprime)
-    #expr = expr.subs(py/pz, yprime)
-    #expr = expr.subs(q*g/pz,k) # Definition from Wille
-    #expr = expr.subs(t, l*m/pz)
-    return expr
-## end zzz
-
-
-
-# select only terms with a certain var in expr, assumes naively that var is the last symbol in each term
-def selectterm(expr, var):
-    strexpr = str(expr)
-    strvar = str(var)
-    idx = strexpr.find(strvar)
-    if idx < 0:
-        return 0
-    if strexpr[idx-1] == " ": # takes care when there is no coefficients
-        return 1
-    if len(strexpr) == len(strvar): # If the only thing in the expression was the var
-        return 1
-    strexpr = strexpr[:idx] # gets rid of the variable and terms to the right (risky way to select the desired term)
-    idx2 = strexpr.rfind(" ")
-    strexpr = strexpr[idx2+1:] 
-    strexpr = strexpr.strip()
-    if len(strexpr) == 0:
-        strexpr = "0"
-    if strexpr.endswith("+") or strexpr.endswith("-") or strexpr.endswith("*") or strexpr.endswith("/"):
-        strexpr = strexpr[:-1] 
-    newexpr = parse_expr(strexpr)
-    return newexpr
-
-def removeprefixones(expr): # still in an early form, BUG: it removes 1.0 if there is a space behind it. BUG: Does not work with several 1.0, especially if there is an early 1.0* then the other 1.0 won't be removed and we're stuck in a loop
-    strexpr = str(expr)
-    idx = 0
-    while True:
-        idx = strexpr.find("1.0")
-        if idx < 0:
-            break
-        if not strexpr[idx+3].isdigit(): # makes sure that for example 1.00002 doesn't get removed
-            strexpr = strexpr[0:idx] + strexpr[idx+4:]
-    newexpr = parse_expr(strexpr)
-    return newexpr
-
-## Matrix construction, only works with my formalism
-#xofl = substitution(transresultqx)
-#print "xofl:", xofl
-#xprimeofl = substitution(transresultpx/pz) # for my formalism
-#xprimeofl = substitution(transresultpx) # for Ems formalism
-#print "xprimeofl:", xprimeofl
-#print "Matrix construction..."
-#m11 = removeprefixones(selectterm(xofl,x0))
-#m12 = removeprefixones(selectterm(xofl,xprime))
-#m21 = removeprefixones(selectterm(xprimeofl,x0))
-#print xprimeofl
-#print selectterm(xprimeofl,xprime)
-#m22 = removeprefixones(selectterm(xprimeofl,xprime))
-#
-#driftmatrix = [[m11, m12], [m21, m22]]
-#print driftmatrix
-
-
-
-
 ########### How many terms in the taylor series... Remake this code so that it finds the optimal order for the run (determinant of the Jacobian differ by less than 10^-5 or 10^-6 from 1) Use and print this order. The suggested changes should be inserted into the element class so that each element has a unique order. Or maybe The optimal order for each pole should be calculated and then just inserted, i.e. 1 for drift and ...
 def findOrder(ham,K,L,acceptableError): # Since this also find the functions maybe it should also return them. Since why do the same thing twice?
     if K*L > 1:
@@ -217,129 +121,11 @@ def findOrder(ham,K,L,acceptableError): # Since this also find the functions may
         order += 1
 
     return order
-#I = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
-#minusI = np.zeros([3,3])-I
-##print minusI
-##print np.dot(I,I)
-#zero = np.zeros([3,3])
-#Supper = np.concatenate((zero,I), axis=1)
-#Sunder = np.concatenate((minusI,zero), axis=1)
-#S = np.concatenate((Supper,Sunder), axis=0)
-##print S
-##print np.dot(S,S) # correct! (-I)
-#
-#sasexp = linalg.expm(S)
-#left = np.dot(S,sasexp)
-#right = np.dot(sasexp,S)
-#
-##print left
-##print right
-##print np.around(left-right) # proves (1.33)
-#
-## jacobian
-#print ''
-#print 'Jacobian...'
-#dxdx = xofl.diff(x0)
-#dxdp = xofl.diff(px)
-#dpdx = xprimeofl.diff(x0)
-#dpdp = xprimeofl.diff(px)
-#
-#print 'dxdx:',dxdx
-#print 'dxdp:',dxdp
-#print 'dpdx:',dpdx
-#print 'dpdp:',dpdp
-#print ''
-#
-## Either det(J) == 1
-#dxdxtimdpdp = (dxdx*dpdp)
-#dxdptimdpdx = (dxdp*dpdx)
-##print 'dxdx*dpdp:',dxdxtimdpdp
-##print 'dxdp*dpdx:',dxdptimdpdx
-##print ''
-#
-#
-## determinant by 'hand'
-#detJ = dxdx*dpdp - dxdp*dpdx
-#detJ = simplify(detJ)
-#print 'detJ =', detJ # when this prints out 1 the simplecity is proved and no further terms are needed
-##J = np.mat('[1 2;3 4]')
-##J = np.mat('[dxdx dxdp; dpdx dpdp]')
-##J = Matrix([[dxdx, dxdp], [dpdx, dpdp]])
-#
-##print J
-#
-## or np.dot(J.transpose(),np.dot(S,J)) == S. Jt*S*J
-## -> dxdx*dpdp - dxdp*dpdx
-#m12 = dxdx*dpdp - dxdp*dpdx
-#m12 = simplify(m12)
-#print 'm12:',m12
-#
-## As long as detJ and m12 gives: 1+ordo(order+1) it should be fine right?
-## But 1+ordo(2) is the result for order 1, can that really work? Shouldn't the order be higher in order to describe the quadrupole?
-#
-########## arbitrary functions and order into lietrans, one dim for simplicity
-#print ''
-#print 'arbitrary functions into lietransformation...'
-#def simplelieop(f,g):
-#    # The old way only one dim
-#    dfdq = f.diff(q) # These four rows can be further developed to support diff for all three qs and three ps
-#    dfdp = f.diff(p)
-#    dgdq = g.diff(q)
-#    dgdp = g.diff(p)
-#    result = dfdq*dgdp-dfdp*dgdq
-#    return result
-#
-### Lie transformation
-## old version
-#def simplelietransform(ham, vof0, t, order): #, zzz
-#    voft = vof0
-#    for i in range(1,order+1):
-#        lieterm = simplify(simplelieop(ham,vof0))
-#        for j in range(0,i-1):
-#            lieterm = simplify(simplelieop(ham,lieterm))
-#
-#        #print "lieterm:",lieterm
-#        #voft = voft + t**i / factorial(i) * lieterm # for my formalism
-#        voft = voft + lieterm / factorial(i) # for Ems formalism
-#
-#    return voft
-### end zzz
-#
-#f = Function('f')(q,p)
-#g = Function('g')(q,p)
-##f = Function('f')(qx,qy,qz,px,py,pz)
-##g = Function('g')(qx,qy,qz,px,py,pz)
-##j = Symbol('j') # order
-#j = 2
-#result = simplelietransform(f, g, t, j) # v[0] is for q # zzz
-#print result
 
-
-
-
-
-
-
-##################### Classes and elements
-## Old functions, zzz
-#def xfunFromHam(ham, order):
-#    transresultqx = lietransform(ham, v[0], t, order)
-#    xofl = substitution(transresultqx)
-#    return xofl
-#
-#def xprimefunFromHam(ham, order):
-#    transresultpx = lietransform(ham, v[3], t, order)
-#    #xprimeofl = simplify(transresultpx/pz) # for my formalism
-#    xprimeofl = simplify(transresultpx) # for Ems formalism
-#    xprimeofl = substitution(xprimeofl)
-#    return xprimeofl
-## end zzz
-
-## New more generalized function
+###### Class for elements
 def funFromHam(ham, order, vof0):
     transresult = lietransform(ham, vof0, order)
-    fun = simplify(transresult) # for Ems formalism
-    #fun = substitution(fun) # For my old formalism
+    fun = simplify(transresult)
     return fun
 
 class Element:
@@ -583,20 +369,15 @@ saveOutput(xoFODO, xpoFODO, yoFODO, ypoFODO)
 
 # Particles should be stored in the output file when they are done to limit memory usage. This might not be optimal for when space charge forces are introduced
 
-# See first comment
-
 # Merge with Anton's code so that I can easily double check that results are correct. New name is needed as well; lie + Accelerator-?code? = ??????? particle tracker?   
 
 # Make a lattice class with funs: add(), evaluate(x,xp,y,yp), __init__()
 
 ##### Comments and stuff not to forget
-# 1. How good the approximation is: Well the criteria is that the determinant of the Jacobian has to be 1. You can decide to truncate at some order with an error that is a fraction of 1 (like 10e-6 or 10e-4).
-# The closer the determinant is to 1 the most accurate will be your approximation. -> make some code to calculate the Jacobian and see how much it differs from 1. Maybe perhaps have it find the lowest order with acceptable errors
+# 1. Seems like the expansion doesn't hold for k > 1...
 
-# 2. Seems like the expansion doesn't hold for k > 1...
+# 2. Changed to a minus sign in front of the qy**2 in the Ham to get proper focus/defocus
 
-# 3. Changed to a minus sign in front of the qy**2 in the Ham to get proper focus/defocus
+# 3. Arguments should always be ordered x,xp,y,yp!
 
-# 4. Arguments should always be ordered x,xp,y,yp!
-
-# 5. Python uses radians, i.e # print cos(3.14/2).evalf()
+# 4. Python uses radians, i.e # print cos(3.14/2).evalf()
