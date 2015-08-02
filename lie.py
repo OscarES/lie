787958d451884c5184478e356f95c9c782bb4d3b
Oscar Elmqvist Sandvik <oscar.elmqvistsandvik@gmail.com>
2015-08-02 15:29:31 +0200

    New plot function as well as the correct envelope function and other minor improvements

--- lie.py
+++ lie.py
@@ -230,33 +230,29 @@ def gaussiantwiss(particles, alpha, beta, epsilon):
 ################## Multiple particles and lattice construction
 print 'Multiple particles and lattice construction...'
 
-def rms(a):
-    sq = np.power(a,2)
-    me = np.mean(sq)
-    root = np.sqrt(me)
-    return root
-
-#def envelopePoint(a):
-#    stda = np.std(a)
-
+#def rms(a):
+#    sq = np.power(a,2)
+#    me = np.mean(sq)
+#    root = np.sqrt(me)
+#    return root
 
 def evalLattice(lattice,(xin,xpin,yin,ypin)):
     xout, xpout, yout, ypout = xin,xpin,yin,ypin
     envx = np.zeros((len(lattice)+1,2))
     envy = np.zeros((len(lattice)+1,2))
 
-    envx[0][1] = rms(xin)
-    envy[0][1] = rms(yin)
+    envx[0][1] = np.std(xin)
+    envy[0][1] = np.std(yin)
 
     elementnbr = 1
 
     for elem in lattice:
         xout, xpout, yout, ypout = elem.evaluate((xout,xpout,yout,ypout))
         envx[elementnbr][0] = envx[elementnbr-1][0] + elem.L
-        envx[elementnbr][1] = rms(xout) # is this really the envelope?
+        envx[elementnbr][1] = np.std(xout)
 
         envy[elementnbr][0] = envy[elementnbr-1][0] + elem.L
-        envy[elementnbr][1] = rms(yout)
+        envy[elementnbr][1] = np.std(yout)
 
         elementnbr += 1
 
@@ -283,27 +279,27 @@ if datamode == 's':
     if len(alpha_x) < 1 : alpha_x = "0"
     alpha_x = float(alpha_x)
     beta_x = raw_input('Enter beta_x:') # set this to 0.001
-    if len(beta_x) < 1 : beta_x = "0.001"
+    if len(beta_x) < 1 : beta_x = "10"
     beta_x = float(beta_x)
     epsilon_x = raw_input('Enter epsilon_x:') # set this to 0.000001
-    if len(epsilon_x) < 1 : epsilon_x = "0.000001"
+    if len(epsilon_x) < 1 : epsilon_x = "0.000000001"
     epsilon_x = float(epsilon_x)
 
     alpha_y = raw_input('Enter alpha_y:') # set this to 0
     if len(alpha_y) < 1 : alpha_y = "0"
     alpha_y = float(alpha_y)
     beta_y = raw_input('Enter beta_y:') # set this to 0.001
-    if len(beta_y) < 1 : beta_y = "0.001"
+    if len(beta_y) < 1 : beta_y = "10"
     beta_y = float(beta_y)
     epsilon_y = raw_input('Enter epsilon_y:') # set this to 0.000001
-    if len(epsilon_y) < 1 : epsilon_y = "0.000001"
+    if len(epsilon_y) < 1 : epsilon_y = "0.000000001"
     epsilon_y = float(epsilon_y)
     
     #x, xp, y, yp = gaussian(nbrofparticles) # uncoupled x and xp, y and yp
     x, xp = gaussiantwiss(nbrofparticles,alpha_x,beta_x,epsilon_x) # coupled x and xp
     y, yp = gaussiantwiss(nbrofparticles,alpha_y,beta_y,epsilon_y) # coupled y and yp
     
-    plotPhaseSpace(x,xp,y,yp)
+    #plotPhaseSpace(x,xp,y,yp)
 
     a = np.zeros(nbrofparticles, dta)
     b = np.zeros(1,dtb)
@@ -336,25 +332,36 @@ elif datamode == 'l':
 
 ##### FODO
 print 'FODO...'
-myKsquared = 0.0001 # Wille and lie formalism, with Ems this is myK
-myK = sqrt(myKsquared)  # Wille and lie formalism, with Ems this is sqrt(myK)
+#myKsquared = 0.0001 # Wille and lie formalism, with Ems this is myK, zzzzz
+myK = 1
+#myK = sqrt(myKsquared)  # Wille and lie formalism, with Ems this is sqrt(myK), zzzzz
 myKfocus = myK
 myKdefocus = -myK # take this times 100 to get an interesting envelope
-myfQuadL = 0.05 # If FODOF cells set this length to half of mydQuadL
-mydQuadL = 0.05
-myDriftL = 2
-mySextuL = 0.05
+myfQuadL = 0.1 # If FODOF cells set this length to half of mydQuadL
+mydQuadL = 0.2
+myDriftL = 0.1
+mySextuK = 1.5
+mySextuL = 0.5
 myOctuL = 0.05
 
 approxOffsetQ = max(abs(x))+3*np.std(x)+max(abs(y))+3*np.std(y)
 approxOffsetP = max(abs(xp))+3*np.std(xp)+max(abs(yp))+3*np.std(yp)
+
+print 'approxOffsetQ:', approxOffsetQ
+print 'approxOffsetP:', approxOffsetP
 
 acceptableError = 1e-9
 driftOrder = findOrder(driftham,0,myDriftL,acceptableError,approxOffsetQ,approxOffsetP)
 quadOrderf = findOrder(quadham,myKfocus,myfQuadL,acceptableError,approxOffsetQ,approxOffsetP)
 quadOrderd = findOrder(quadhamdefocus,myKdefocus,mydQuadL,acceptableError,approxOffsetQ,approxOffsetP)
 
-#sextuOrder = findOrder(sextupoleham,myK,10*myfQuadL,acceptableError)
+print 'driftOrder:',driftOrder
+print 'quadOrderf:',quadOrderf
+print 'quadOrderd:',quadOrderd
+
+sextuOrder = findOrder(sextupoleham,mySextuK,mySextuL,acceptableError,approxOffsetQ,approxOffsetP)
+
+print 'sextuOrder:',sextuOrder
 
 
 fF = Element('quadfocus', quadham, myKfocus, myfQuadL, quadOrderf)
@@ -364,23 +371,30 @@ oO2 = Element('drift', driftham, 0, myDriftL, driftOrder)
 
 
 ## higher order elements
-#sextupole = Element('sextupole', sextupoleham, myK, mySextuL, order)
+sextupole = Element('sextupole', sextupoleham, mySextuK, mySextuL,sextuOrder)
 #octupole = Element('octupole', octupoleham, myK, myOctuL, order)
 
 
 fodoLattice = list()
 nbroffodos = raw_input('Enter number of FODO cells:')
-if len(nbroffodos) < 1 : nbroffodos = "10"
+if len(nbroffodos) < 1 : nbroffodos = "50"
 nbroffodos = int(nbroffodos)
 for i in range(nbroffodos):
     fodoLattice.append(fF)
     fodoLattice.append(oO1)
+    fodoLattice.append(oO1)
+    fodoLattice.append(oO1)
+    fodoLattice.append(oO1)
     fodoLattice.append(dD)
     fodoLattice.append(oO2)
+    fodoLattice.append(oO2)
+    fodoLattice.append(oO2)
+    fodoLattice.append(oO2)
 
-    #fodoLattice.append(fF)
+    fodoLattice.append(fF)
 
-    #fodoLattice.append(sextupole)
+    #if i%10 == 0: fodoLattice.append(sextupole)
+    fodoLattice.append(sextupole)
     #fodoLattice.append(octupole)
 
 # input from randoms and loads above
@@ -404,8 +418,57 @@ def plotEnvelope(envx,envy):
 
     plt.show()
 
-plotPhaseSpace(xoFODO, xpoFODO, yoFODO, ypoFODO)
-plotEnvelope(envx,envy)
+
+def plotEverything(xin,xpin,yin,ypin,alpha_x,beta_x,epsilon_x,alpha_y,beta_y,epsilon_y,xo,xpo,yo,ypo,envx,envy):
+    plt.figure(0)
+    ax1 = plt.subplot2grid((4,3), (0,0))
+    plt.plot(xin,xpin,'ro')
+    plt.title('Initial value in x')
+    plt.xlabel('x')
+    plt.ylabel('xp')
+    ax2 = plt.subplot2grid((4,3), (0,1))
+    plt.plot(yin,ypin,'bo')
+    plt.title('Initial value in y')
+    plt.xlabel('y')
+    plt.ylabel('yp')
+    ax3 = plt.subplot2grid((4,3), (0,2))
+    plt.plot(xin,yin,'go')
+    plt.title('Initial values x and y')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    ax4 = plt.subplot2grid((4,3), (1, 0), colspan=3)
+    plt.plot(envx[:,0],envx[:,1],'ro')
+    plt.title('Envelope in x by z')
+    plt.xlabel('z')
+    plt.ylabel('Envelope in x')
+    ax5 = plt.subplot2grid((4,3), (2, 0), colspan=3)
+    plt.plot(envy[:,0],envy[:,1],'bo')
+    plt.title('Envelope in y by z')
+    plt.xlabel('z')
+    plt.ylabel('Envelope in y')
+    ax6 = plt.subplot2grid((4,3), (3, 0))
+    plt.plot(xo,xpo,'ro')
+    plt.title('Values after FODO in x')
+    plt.xlabel('x')
+    plt.ylabel('xp')
+    ax7 = plt.subplot2grid((4,3), (3, 1))
+    plt.plot(yo,ypo,'bo')
+    plt.title('Values after FODO in y')
+    plt.xlabel('y')
+    plt.ylabel('yp')
+    ax8 = plt.subplot2grid((4,3), (3, 2))
+    plt.plot(xo,yo,'go')
+    plt.title('Values after FODO in y and x')
+    plt.xlabel('x')
+    plt.ylabel('y')
+
+    plt.suptitle("Plots")
+    plt.show()
+
+#plotPhaseSpace(xoFODO, xpoFODO, yoFODO, ypoFODO)
+#plotEnvelope(envx,envy)
+
+plotEverything(x,xp,y,yp,alpha_x,beta_x,epsilon_x,alpha_y,beta_y,epsilon_y,xoFODO,xpoFODO,yoFODO,ypoFODO,envx,envy)
 
 ######## Output
 def saveOutput(x,xp,y,yp,alpha_x,beta_x,epsilon_x,alpha_y,beta_y,epsilon_y):
